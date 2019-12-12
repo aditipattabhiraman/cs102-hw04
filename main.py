8b6920ada8ebdba990a3ea0a21a60baa5bb3e018
Aditi Pattabhiraman <aditi.pattabhiraman@cooper.edu>
2019-12-11 19:39:52 -0500

    Black and White Filter with White Borders

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 from PIL import Image
 
@@ -9,24 +10,12 @@ output_path = sys.argv[2]
 img = Image.open(input_path)
 width, height = img.size
 
-# Create a new, all-white image that's the same size as the original
 new_img = Image.new("RGB", (width, height), "white")
 
-# TODO: Replace this with your own filter!
-# Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [(0, 0)] * 9
-for i in range(1, width - 1):
+for i in range(width // 4, (width * 3) // 4):
     for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        members.sort()
-        new_img.putpixel((i, j), (members[4]))
+        r, g, b = img.getpixel((i, j))
+        avg = (r + g + b) // 3
+        new_img.putpixel((i, j), (avg, avg, avg))
 
-new_img.save(output_path, "JPEG")
+new_img.save(output_path)
